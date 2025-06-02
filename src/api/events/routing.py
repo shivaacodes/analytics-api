617334199d67dabf0aa2f73bd01c86468000e8ee
Shiva Sajay <shivasajay007@gmail.com>
2025-06-02 20:14:44 +0530

    tested all endpoints

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,18 +1,32 @@
 from fastapi import APIRouter
-from .schemas import EventSchema, EventListSchema
+from .schemas import (
+    EventSchema, 
+    EventListSchema, 
+    EventCreateSchema,
+    EventUpdateSchema
+)
 
 router = APIRouter()
 
+#Get aLL Evnets
 @router.get("/", response_model=EventListSchema)
 def read_events() -> EventListSchema:
     events = [EventSchema(id=i) for i in [1, 2, 3, 4, 5]]
     return EventListSchema(results=events, count=len(events))
 
-@router.post("/", response_model=EventSchema)
-def create_event(data:dict={}) -> EventSchema:
-    print(data)
-    return EventSchema(id=125)
-
+#Get a specific event
 @router.get("/{event_id}", response_model=EventSchema)
 def get_event(event_id: int) -> EventSchema:
     return EventSchema(id=event_id)
+
+#Create a New Event
+@router.post("/", response_model=EventSchema)
+def create_event(payload:EventCreateSchema) -> EventSchema:
+    print(type(payload.page))
+    return EventSchema(id=125,page=payload.page)
+
+# Update this id :/api/events/12
+@router.put("/{event_id}")
+def update_event(event_id:int, payload:EventUpdateSchema)->EventSchema:
+    print(payload.description)
+    return EventSchema(id=event_id,description=payload.description)
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,11 +1,25 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+"""
+id
+path
+description
+"""
 
 # Design of the database models
 class EventSchema(BaseModel):
     id: int
+    page:Optional[str]=""
+    description:Optional[str]=Field(default="My description")
 
 
 class EventListSchema(BaseModel):
     results:List[EventSchema]
     count:int
+
+class EventCreateSchema(BaseModel):
+    page:str
+
+class EventUpdateSchema(BaseModel):
+    description:str
